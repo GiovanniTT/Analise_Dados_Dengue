1fbf67cfad6e9efebe5f5d6084ba89e051d2afde
Giovanni Torres <gi-torres1@hotmail.com>
2026-01-27 15:49:14 -0300

    Análise estatística realizada

--- analise.py
+++ analise.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import numpy as np
+
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 
 url = URL.create(
     drivername="mysql+pymysql",
     username="giovanni",
-    password="Senha@123",  # aqui pode usar normal
+    password="Senha@123",
     host="localhost",
     port=3306,
     database="dengue_db"
@@ -17,10 +19,49 @@ df = pd.read_sql(
     """
     SELECT *
     FROM dengue_dados
-    ORDER BY ano, mes
-    LIMIT 5
+    ORDER BY estado, ano, mes
     """,
     engine
 )
 
-print(df)
+for col in ['casos']:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
+df['casos_movel_12'] = (
+    df.groupby('estado')['casos']
+      .transform(lambda x: x.rolling(12, min_periods=1).mean())
+)
+
+df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+p50 = df['casos_movel_12'].quantile(0.50)
+p75 = df['casos_movel_12'].quantile(0.75)
+p90 = df['casos_movel_12'].quantile(0.90)
+
+def classificar_risco(valor):
+    if valor <= p50:
+        return 'Baixo'
+    elif valor <= p75:
+        return 'Médio'
+    elif valor <= p90:
+        return 'Alto'
+    else:
+        return 'Epidêmico'
+
+df['nivel_risco'] = df['casos_movel_12'].apply(classificar_risco)
+
+print("\nAMOSTRA DOS DADOS ENRIQUECIDOS")
+print(df.head(10))
+
+model_df = df.dropna(subset=[
+    'casos',
+    'temperatura',
+    'precipitacao',
+    'casos_movel_12'
+])
+
+"""
+df.to_sql("dengue_dados_enriquecidos", engine, if_exists="replace", index=False)
+corr_df.to_sql("correlacao_casos_clima_estado", engine, if_exists="replace", index=False)
+coef_df.to_sql("modelo_coeficientes", engine, if_exists="replace", index=False)
+"""
